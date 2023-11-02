370bd224693e8c87b9f30f2b7eca6e3db0b29d81
tam-dcreator <tam@THE-7TH>
2023-11-03 00:55:33 +0100

    Amended Function that measures the balance factor of a binary tree

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -18,7 +18,7 @@ int binary_tree_balance(const binary_tree_t *tree)
 
 	l_sub_height = tree->left ? 1 + (binary_tree_height(tree->left)) : 0;
 
-        r_sub_height = tree->right ? 1 + (binary_tree_height(tree->right)) : 0;
+	r_sub_height = tree->right ? 1 + (binary_tree_height(tree->right)) : 0;
 
 	return (l_sub_height - r_sub_height);
 }
@@ -31,13 +31,13 @@ int binary_tree_balance(const binary_tree_t *tree)
 */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-        size_t l_height, r_height;
+	size_t l_height, r_height;
 
-        if (!tree)
-                return (0);
+	if (!tree)
+		return (0);
 
-        l_height = tree->left ? 1 + binary_tree_height(tree->left) : 0;
-        r_height = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+	l_height = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+	r_height = tree->right ? 1 + binary_tree_height(tree->right) : 0;
 
-        return ((l_height > r_height) ? l_height : r_height);
+	return ((l_height > r_height) ? l_height : r_height);
 }
