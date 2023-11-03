7908e43dacb10eeb03e4213cfc4c544d2fcac100
tam-dcreator <tam@THE-7TH>
2023-11-03 01:56:31 +0100

    Amended Function that checks if a binary tree is perfect

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -11,7 +11,8 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 	if (!tree)
 		return (0);
 
-	if (binary_tree_is_full(tree) && (binary_tree_balance(tree) == 0))
+	if ((binary_tree_is_full(tree) == 1)
+	    && (binary_tree_balance(tree) == 0))
 		return (1);
 	return (0);
 }
